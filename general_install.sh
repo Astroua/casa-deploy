ab9e153b6a941ebedf22db38664732983294bcfb
e-koch <koch.eric.w@gmail.com>
2015-08-22 16:58:20 -0700

    Extend general install

--- general_install.sh
+++ general_install.sh
@@ -2,5 +2,5 @@
 # General programs needed for VMs
 
 sudo apt-get update
-sudo apt-get install xorg openbox wget bzip2 ca-certificates git
+sudo apt-get install -y xorg openbox wget bzip2 ca-certificates git unzip
 
