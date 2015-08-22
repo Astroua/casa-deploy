fb4e74c18c76342c488d4275f6fe49a93b4e2fd5
e-koch <koch.eric.w@gmail.com>
2015-08-22 16:58:34 -0700

    Use general install script

--- deploy_casa4.3.sh
+++ deploy_casa4.3.sh
@@ -1,11 +1,16 @@
+# License under the MIT License - see LICENSE
 
 # Install CASA 4.3 on a debian based system
 
+# General software install
+sh $HOME/casa-deploy/general_install.sh
+
+# CASA specifics install
 sudo apt-get update
-sudo apt-get install -y wget bzip2 ca-certificates \
-    libglib2.0-0 libxext6 libsm6 libxrender1 libpng-dev libfreetype6 libfreetype6-dev libxi6 libxrandr2 \
-    libxfixes3 libxcursor1 libxinerama1 libfontconfig1 libsqlite3-0 libxslt1.1 unzip bsdmainutils \
-    libcurl4-openssl-dev libxft2 xorg openbox
+sudo apt-get install -y  \
+    libglib2.0-0 libxext6 libsm6 libxrender1 libpng-dev libfreetype6 libfreetype6-dev libxi6 \
+    libxrandr2 libxfixes3 libxcursor1 libxinerama1 libfontconfig1 libsqlite3-0 libxslt1.1 \
+    bsdmainutils libcurl4-openssl-dev libxft2
 
 wget --quiet https://svn.cv.nrao.edu/casa/linux_distro/release/el6/old/casa-release-4.3.1-el6.tar.gz
 
