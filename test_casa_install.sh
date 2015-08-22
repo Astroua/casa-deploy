4b5143c95dff3cf325bd537d1cb0e496b181d789
e-koch <koch.eric.w@gmail.com>
2015-08-22 16:51:04 -0700

    Test the install by running a simple reduction

---
+++ test_casa_install.sh
@@ -0,0 +1,11 @@
+
+# Test the casa install by running a reduction script with test data included in CASA
+
+mkdir test_reduction
+cd test_reduction
+
+# Run the script (in non-interactive mode)
+casa --no-logger -c ngc5921_demo.py False
+
+# Add checks for certain files in the output
+# TODO!!
