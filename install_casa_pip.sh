6aeb35a941e076eb07e5a1cd608cac946fb4f334
e-koch <koch.eric.w@gmail.com>
2015-09-24 18:13:23 -0600

    Script for casa-pip install

---
+++ install_casa_pip.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# License under the MIT License - see LICENSE
+
+cd $HOME
+
+# Install casa-pip
+git clone https://github.com/radio-astro-tools/casa-python.git
+
+python casa-python/setup_casapy_pip.py
+
+echo "# casa-pip" >> $HOME/.bashrc
+echo 'export PATH=$PATH:$HOME/.casa/bin' >> $HOME/.bashrc
+
+source $HOME/.bashrc
