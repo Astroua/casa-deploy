b6f0a91ea9b66386522260ca43514ff42bfeaa67
e-koch <koch.eric.w@gmail.com>
2015-09-29 14:59:15 -0600

    Should be upgrade

--- install_casa_packages.sh
+++ install_casa_packages.sh
@@ -2,7 +2,7 @@
 # Install packages into CASA using casa-pip
 
 casa-pip install astropy
-casa-pip install numpy --update
-casa-pip install scipy --update
-casa-pip install matplotlib --update
+casa-pip install numpy --upgrade
+casa-pip install scipy --upgrade
+casa-pip install matplotlib --upgrade
 casa-pip install jdcal
