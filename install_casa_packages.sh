3fee3cb1e7122bdc5dae20987b7f469d118f36a3
e-koch <koch.eric.w@gmail.com>
2015-09-29 14:39:34 -0600

    Add some packages to install into CASA

--- install_casa_packages.sh
+++ install_casa_packages.sh
@@ -1,2 +1,8 @@
 
 # Install packages into CASA using casa-pip
+
+casa-pip install astropy
+casa-pip install numpy --update
+casa-pip install scipy --update
+casa-pip install matplotlib --update
+casa-pip install jdcal
