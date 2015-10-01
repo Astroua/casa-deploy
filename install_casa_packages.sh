d1e64e7f7f7a8900897b6a37cb7cf52e312aa7b0
e-koch <koch.eric.w@gmail.com>
2015-10-01 17:54:23 -0600

    Add more packages to install into CASA

--- install_casa_packages.sh
+++ install_casa_packages.sh
@@ -2,6 +2,8 @@
 # Install packages into CASA using casa-pip
 
 casa-pip install astropy
+casa-pip install photoutils
+casa-pip install scikit-image
 # casa-pip install numpy --upgrade
 # casa-pip install scipy --upgrade
 # casa-pip install matplotlib --upgrade
