64e71a22225e1b298e6feabfcf5e976bb9a9be84
e-koch <koch.eric.w@gmail.com>
2015-10-28 10:46:10 -0600

    Add conda installs

--- install_aegean.sh
+++ install_aegean.sh
@@ -4,10 +4,13 @@
 
 # Installs the Aegean source finder to the Miniconda environment
 # https://github.com/PaulHancock/Aegean
+# Miniconda MUST be installed!
 
 cd $HOME
 
 git clone https://github.com/PaulHancock/Aegean.git
+
+conda install -y scipy lmfit astropy blist
 
 echo "# Aegean" >> $HOME/.bashrc
 echo "PATH="$PATH:$HOME/Aegean/"
